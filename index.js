4a5ed101f054f6b43d7db20e4c2ddb9577974390
Maulana Malik Ibrahim <93629656+ibrammalik@users.noreply.github.com>
2022-08-04 08:11:03 +0700

    Update index.js
    Passed Test, Challange Completed

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress, language, software })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
